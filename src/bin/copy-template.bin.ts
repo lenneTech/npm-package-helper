9a640e22f5cae8fa4487f5b90d9157e6eb71ff9f
Kai Haase <kai.haase@lenne.tech>
2019-09-01 10:46:12 +0200

    Optimized copy template script

--- src/bin/copy-template.bin.ts
+++ src/bin/copy-template.bin.ts
@@ -7,34 +7,35 @@ import NpmPackageHelper from '..';
  */
 const run = async () => {
   try {
-    // Path to file
-    const versionSyncPath = join(process.cwd(), 'extras', 'sync-version.ts');
-
-    // Create directory
-    await fs.mkdir(dirname(versionSyncPath));
-
-    // Copy file
-    await fs.copyFile(
-      join(__dirname, '..', '..', 'templates', 'run.template.ts'),
-      versionSyncPath,
-      constants.COPYFILE_EXCL
-    );
-
     // Integrate into package.json
     const { data, path } = await NpmPackageHelper.getFile('package.json', {
-      cwd: process.cwd()
+      cwd: __dirname
     });
-    if (
-      path &&
-      data &&
-      data.husky &&
-      data.husky.hooks &&
-      data.husky.hooks['pre-commit'] &&
-      !data.husky.hooks['pre-commit'].includes('sync-version')
-    ) {
-      data.husky.hooks['pre-commit'] =
-        'ts-node extras/sync-version.ts && ' + data.husky.hooks['pre-commit'];
-      await NpmPackageHelper.setFile(path, data);
+    if (path && data) {
+      // Path to file
+      const versionSyncPath = join(dirname(path), 'extras', 'sync-version.ts');
+
+      // Create directory
+      await fs.mkdir(dirname(versionSyncPath));
+
+      // Copy file
+      await fs.copyFile(
+        join(__dirname, '..', '..', 'templates', 'run.template.ts'),
+        versionSyncPath,
+        constants.COPYFILE_EXCL
+      );
+
+      if (
+        data.husky &&
+        data.husky.hooks &&
+        data.husky.hooks['pre-commit'] &&
+        !data.husky.hooks['pre-commit'].includes('sync-version')
+      ) {
+        data.husky.hooks['pre-commit'] =
+          'ts-node --skip-project extras/sync-version.ts && ' +
+          data.husky.hooks['pre-commit'];
+        await NpmPackageHelper.setFile(path, data);
+      }
     }
   } catch (e) {
     if (e && e.code !== 'EEXIST') {
